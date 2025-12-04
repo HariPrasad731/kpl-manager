2d897dffd1ac21373cb4480eca88477628a01ed4
Mahesh Agathamudi <maheshagathamudi@gmail.com>
2025-12-04 15:26:52 +0530

    kpl-manager

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/kpl-manager/",  // 👈 ADD THIS LINE
+  // use relative paths so it works on GitHub Pages
+  base: './',
+  // build directly into /docs for GitHub Pages
+  build: {
+    outDir: 'docs',
+  },
 })
